a2df00ad80237a06c465b774940074250800cbef
Oren Dinur <orendinurbs@gmail.com>
2023-09-15 14:19:09 +0300

    centered genres

--- fsm-home-assigment/src/components/BooksLayout/BooksLayout.jsx
+++ fsm-home-assigment/src/components/BooksLayout/BooksLayout.jsx
@@ -14,15 +14,17 @@ export const BooksLayout = () => {
   const onClick = () => {};
 
   return (
-    <ul className={styles.list}>
-      {bookCategories.map((category, index) => (
-        <li key={index} className={styles.item}>
-          <div className={styles.button} onClick={onClick}>
-            {category}
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div className={styles.container}>
+      <ul className={styles.list}>
+        {bookCategories.map((category, index) => (
+          <li key={index} className={styles.item}>
+            <div className={styles.button} onClick={onClick}>
+              {category}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
 
     // <BookList genre={"fiction"} />;
   );
